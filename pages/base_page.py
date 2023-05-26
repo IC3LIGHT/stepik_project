b27b7909b3984802e344be5f624e1c9658ab711e
IC3LIGHT <112938558+IC3LIGHT@users.noreply.github.com>
2023-05-26 17:14:31 +0300

    methods for the BasePage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -15,10 +15,6 @@ class BasePage:
     def open(self):
         self.browser.get(self.url)
 
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
-
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
